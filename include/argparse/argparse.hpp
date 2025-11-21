b6b7b9237e0880e2150f380d29ed45b92137ae52
Leander Schulten <Leander.Schulten@tetys.de>
2025-11-21 18:25:28 +0100

    inline radix

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -159,10 +159,10 @@ template <> constexpr bool standard_unsigned_integer<unsigned long int> = true;
 template <>
 constexpr bool standard_unsigned_integer<unsigned long long int> = true;
 
-constexpr int radix_2 = 2;
-constexpr int radix_8 = 8;
-constexpr int radix_10 = 10;
-constexpr int radix_16 = 16;
+inline constexpr int radix_2 = 2;
+inline constexpr int radix_8 = 8;
+inline constexpr int radix_10 = 10;
+inline constexpr int radix_16 = 16;
 
 template <typename T>
 constexpr bool standard_integer =
